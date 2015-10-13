a3784e400ba9c324db54ece987de0d3649367710
Joe Lanman <joelanman@gmail.com>
2015-10-13 18:30:41 +0100

    updated templates to use nunjucks

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ app.get(/^\/([^.]+)$/, function (req, res) {
 
 	res.render(path, function(err, html) {
 		if (err) {
-			res.render(path + "/index", function(err, html) {
-        if (err) {
+			res.render(path + "/index", function(err2, html) {
+        if (err2) {
           console.log(err);
-          res.sendStatus(404);
+          res.status(404).send(err).send(err2);
         } else {
           res.end(html);
         }
