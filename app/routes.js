3d671756d6899c5387ac7494d42919d3c60b8123
Joe Lanman <joelanman@gmail.com>
2015-11-06 15:22:31 +0000

    started rendering docs

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var marked = require('marked');
+var fs = require('fs');
+var path = require('path');
 
 router.get('/', function (req, res) {
   res.render('index');
+});
+
+router.get('/docs/:page', function (req, res) {
+  var doc = fs.readFileSync(path.join(__dirname, "../docs/" + req.params.page + ".md"), "utf8");
+  console.log(doc);
+  var html = marked(doc);
+  res.render("docs", {"document": html});
 });
 
 router.get('/examples/template-data', function (req, res) {
