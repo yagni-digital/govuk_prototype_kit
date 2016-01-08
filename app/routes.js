69fad94599375755bfb90b64dc2314a11480ef63
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2016-01-08 11:43:32 +0000

    Add tutorial files

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,30 @@ router.get('/examples/template-data', function (req, res) {
   res.render('examples/template-data', { 'name' : 'Foo' });
 });
 
-// add your routes here
+// Ineligible users are routed to '/ineligble.html'
+
+router.get('/question-2', function (req, res) {
+
+  var eligible = req.query.eligible;
+
+  if (eligible == "Yes"){
+    res.render('question-2');
+  } else {
+    res.render('ineligible');
+  }
+  
+});
+
+// Write the user input to the check your answers page
+
+router.get('/check-your-answers-page', function (req, res) {
+
+  var feat = req.query.feat;
+
+  res.render('check-your-answers-page', { 'feat' : feat });
+  
+});
+
+
 
 module.exports = router;
